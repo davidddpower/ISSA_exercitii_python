c474d2af55faf8ff9e08ce514ae346296e20b8ce
Apavaloae-Lehonschi David <147439533+davidddpower@users.noreply.github.com>
2026-07-16 09:43:17 +0300

    Implement function to print list elements
    Added a function to print elements of a list.

---
+++ Intro/Ex10.py
@@ -0,0 +1,8 @@
+def print_list_elements(items):
+    # Parcurge fiecare element din lista
+    for element in items:
+        # Afiseaza elementul curent
+        print(element)
+
+# Exemplu de utilizare
+print_list_elements([1, 3, 5, 7])
